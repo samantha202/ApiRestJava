092148f765e19d6ad20c668006e7319070e28618
noussi <noussi@192.168.1.74>
2021-10-13 21:22:33 +0200

    verb http Get implemented

--- src/main/java/net/codejava/ws/MovieRessource.java
+++ src/main/java/net/codejava/ws/MovieRessource.java
@@ -1,5 +1,43 @@
 package net.codejava.ws;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+@Path("movies")
 public class MovieRessource {
-
+ private MovieDOA dao =  MovieDOA.getInstance();
+ // this verb http returns the list of movies
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Movie> list() 
+	{
+		return dao.listAll();
+	}
+//this http verb returns a movies according to the id	
+	@GET
+	@Path("{id}")
+	public Response get(@PathParam("id") int id) {
+		Movie movie = dao.get(id);
+		if(movie != null) {
+			return Response.ok(movie, MediaType.APPLICATION_JSON).build();
+		}else {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+	}
+//this verb http returns a films according to the city of projection
+	@GET
+	@Path("vil/{ville}")
+	public Response get(@PathParam("ville") String ville) {
+		List<Movie> movie = dao.GetTown(ville);
+		if(movie != null) {
+			return Response.ok(movie, MediaType.APPLICATION_JSON).build();
+		}else {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+	}
 }
+
